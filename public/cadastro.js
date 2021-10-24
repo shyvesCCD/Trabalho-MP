f9bb23c5b33e0cf0f864893b8b1a8bbcc9ff1c83
shyvesCCD <pedroochavess97@gmail.com>
2021-10-23 22:21:31 -0300

    criando usuario com sucesso.

--- public/cadastro.js
+++ public/cadastro.js
@@ -1,11 +1,19 @@
-const url = "http://localhost:3000/create";
+const url = "http://localhost:3333/users";
 
 const button = document.getElementById("cadastrar");
 button.addEventListener("click", function (e) {
+  alert("oi");
   e.preventDefault();
   const usuario = document.getElementById("usuario").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("senha").value;
 
-  fetch(url, { method: 'post', body: usuario, email, password})
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nickname: usuario, email: email, senha: password }),
+  });
 });
